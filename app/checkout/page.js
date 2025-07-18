91c94ae9fe5a5f97f00cb496eadc7bafe5804ccc
Melvin-js <melvinjs404@gmail.com>
2025-07-18 21:40:09 +0530

    bug fix 1.1

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,10 +1,4 @@
-'use client';
 
-import { useState, useEffect, useRef } from 'react';
-import styles from './style.module.css';
-import Image from 'next/image';
-import DailyTreatsBanner from '.dailyTreats/components/banner';
-import PreBookBanner from '.prebook/components/banner';
 
 export default function CheckoutPage() {
 
